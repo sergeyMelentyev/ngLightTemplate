0d6b42234df238171513fc3b62a5f5f4f1f88eed
Sergey Melentyev <sergey.melentyev@gmail.com>
2015-09-05 21:16:27 +0300

    new version

--- js/anim/anim.js
+++ js/anim/anim.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var appWrapper = $('.appWrapper'),
+var preLoaderWrapper = $('.preLoaderWrapper'),
 	centerLevelText = $('.centerLevelText');
 
 function removeFirstLoader(){
-	appWrapper.transition({ scale: 0.9 }, 250, 'cubic-bezier(0, 0, 0, 0.69)')
+	preLoaderWrapper.transition({ scale: 0.9 }, 250, 'cubic-bezier(0, 0, 0, 0.69)')
 			  .transition({ x: 2000 }, 1000, 'cubic-bezier(0, 0, 0, 0.69)', function(){
-			  	appWrapper.css({'visibility': 'hidden'});
+			  	preLoaderWrapper.css({'visibility': 'hidden'});
 			  });
 };
